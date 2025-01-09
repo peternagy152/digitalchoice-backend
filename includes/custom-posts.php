6268d1c948a947de10c85eb76e12c40267c0eb02
Peter <peter.nagy152@gmail.com>
2025-01-09 02:09:46 +0200

    Project Category

--- includes/custom-posts.php
+++ includes/custom-posts.php
@@ -158,3 +158,35 @@ function theme_settings()
 	register_post_type('theme-settings', $args);
 }
 add_action('init', 'theme_settings');
+
+
+
+add_action('init', 'register_project_category_taxonomy', 0);
+function register_project_category_taxonomy()
+{
+
+	$labels = array(
+		'name'              => _x('Category', 'taxonomy general name'),
+		'singular_name'     => _x('Category', 'taxonomy singular name'),
+		'search_items'      => __('Search Category'),
+		'all_items'         => __('All Category'),
+		'parent_item'       => __('Parent Category'),
+		'parent_item_colon' => __('Parent :Category'),
+		'edit_item'         => __('Edit Category'),
+		'update_item'       => __('Update Category'),
+		'add_new_item'      => __('Add New Category'),
+		'new_item_name'     => __('New Category'),
+		'menu_name'         => __('Category'),
+	);
+
+	$args = array(
+		'hierarchical'      => true,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array('slug' => 'project_category'),
+	);
+
+	register_taxonomy('project_category', 'projects', $args);
+}
